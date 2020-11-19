51835ebfe8ef653a0429a02aa7e526e8fb17e518
Daniel McGough <daniel.e.mcgough@avanade.com>
2020-11-19 12:25:43 -0800

    chore(migration): cleanup and testing * computed callbacks moved to helpers file * spec updates

---
+++ frontend/tests/unit/helpers/index.spec.js
@@ -0,0 +1,36 @@
+import { setNextMonth, setPrevMonth, setParams } from '../../../src/helpers'
+
+describe('setNextMonth', () => {
+  it('returns first month and next year', () => {
+    const monthYear = { month: 11, year: 2020 }
+    const expected = { month: 1, year: 2021 }
+    expect(setNextMonth(monthYear)).toStrictEqual(expected)
+  })
+  it('returns correct month and correct year', () => {
+    const monthYear = { month: 9, year: 2020 }
+    const expected = { month: 11, year: 2020 }
+    expect(setNextMonth(monthYear)).toStrictEqual(expected)
+  })
+})
+
+describe('setPrevMonth', () => {
+  it('returns last month and previous year', () => {
+    const monthYear = { month: 0, year: 2020 }
+    const expected = { month: 12, year: 2019 }
+    expect(setPrevMonth(monthYear)).toStrictEqual(expected)
+  })
+  it('returns correct month and correct year', () => {
+    const monthYear = { month: 10, year: 2020 }
+    const expected = { month: 10, year: 2020 }
+    expect(setPrevMonth(monthYear)).toStrictEqual(expected)
+  })
+})
+
+describe('setParams', () => {
+  it('returns year and month as numbers and correctly', () => {
+    const monthYear = { month: 2, year: 2020 }
+    const { month, year } = setParams(monthYear)
+    expect(month).toEqual(1)
+    expect(year).toEqual(2020)
+  })
+})
