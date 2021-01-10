e81357bbbe4c62e51fb8c526b7683afcca316f21
Daniel McGough <daniel.e.mcgough@avanade.com>
2021-01-10 15:49:57 -0800

    refactor: useCalendarApi * no longer async since not run in worker

--- frontend/src/composables/useCalendarApi.js
+++ frontend/src/composables/useCalendarApi.js
@@ -1,11 +1,11 @@
 import { calendarApi } from '@/workers/calendar-api'
 
-export async function useCalendarApi(randomCount) {
+export function useCalendarApi(randomCount) {
   let api
 
   if (api) return api
 
-  api = await calendarApi(randomCount)
+  api = calendarApi(randomCount)
 
   return api
 }
