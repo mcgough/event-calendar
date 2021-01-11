43610378cd97ee8c147f8099a080a5f2a6a63be2
Daniel McGough <daniel.e.mcgough@avanade.com>
2021-01-10 16:42:26 -0800

    refactor: generateRandomEvents

--- backend/utils/index.js
+++ backend/utils/index.js
@@ -1,27 +1,25 @@
-/**
- * For testing purposes
- */
-
 const getDay = require('date-fns/getDay')
 const getWeekOfMonth = require('date-fns/getWeekOfMonth')
 
+function rangeMap(handler) {
+  return function (length) {
+    return Array.from(length).map(handler)
+  }
+}
+
+function formatEvent(_, i) {
+  const start_time = Date.now() / 1000 + i * 15555555
+  const end_time = start_time + 100000
+  return {
+    title: `Event ${i}`,
+    when: { start_time, end_time },
+    day: getDay(start_time),
+    week: getWeekOfMonth(start_time, { weekStartsOn: 0 }),
+  }
+}
+
 module.exports = {
   generateRandomEvents(count) {
-    const random = []
-    for (let i = 0; i < count; i++) {
-      const start_time = Date.now() / 1000 + i * 15555555
-      const end_time = start_time + 100000
-      const event = {
-        title: `Event ${i}`,
-        when: {
-          start_time,
-          end_time,
-        },
-        day: getDay(start_time),
-        week: getWeekOfMonth(start_time, { weekStartsOn: 0 }),
-      }
-      random.push(event)
-    }
-    return random
+    return rangeMap(formatEvent)(count)
   },
 }
