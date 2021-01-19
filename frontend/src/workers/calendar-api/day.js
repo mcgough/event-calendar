4674355be23d89f248cee287f29f2f6297e91953
Daniel McGough <daniel.e.mcgough@avanade.com>
2021-01-18 16:05:35 -0800

    fix: Day Month sub view nav edge cases

--- frontend/src/workers/calendar-api/day.js
+++ frontend/src/workers/calendar-api/day.js
@@ -1,5 +1,41 @@
 import compose from 'lodash.flowright'
-import { format, getTime, isToday } from 'date-fns'
+import {
+  format,
+  getTime,
+  isToday,
+  isFirstDayOfMonth,
+  isLastDayOfMonth,
+} from 'date-fns'
+
+export function Day({ y, m, dayOfMonth, dayOfWeek, isInCurrentMonth }) {
+  const year = y
+  const month = m
+  const timestamp = parseInt(format(new Date(y, m, dayOfMonth), 'T'), 10)
+  const label = format(timestamp, 'PPP')
+  const isFirstDay = isFirstDayOfMonth(timestamp)
+  const isLastDay = isLastDayOfMonth(timestamp)
+
+  let events = []
+  const eventCount = () => events.length
+  const getEvents = () => [...events]
+  const setEvent = (e) => (events = setEvents(e, events))
+
+  return {
+    year,
+    month,
+    dayOfMonth,
+    dayOfWeek,
+    eventCount,
+    getEvents,
+    isFirstDay,
+    isLastDay,
+    isInCurrentMonth,
+    label,
+    setEvent,
+    timestamp,
+    isToday: isToday(timestamp),
+  }
+}
 
 const formatDateString = (string) => string.replace(/-/g, '/')
 
@@ -23,30 +59,4 @@ export function addEvent(findFn) {
 
 export function pluckDay({ data, value }) {
   return { data, value: value.findDay(data) }
-}
-
-export function Day({ y, m, dayOfMonth, dayOfWeek, isInCurrentMonth }) {
-  const year = y
-  const month = m
-  const timestamp = parseInt(format(new Date(y, m, dayOfMonth), 'T'), 10)
-  const label = format(timestamp, 'PPP')
-
-  let events = []
-  const eventCount = () => events.length
-  const getEvents = () => [...events]
-  const setEvent = (e) => (events = setEvents(e, events))
-
-  return {
-    year,
-    month,
-    dayOfMonth,
-    dayOfWeek,
-    eventCount,
-    getEvents,
-    isInCurrentMonth,
-    label,
-    setEvent,
-    timestamp,
-    isToday: isToday(timestamp),
-  }
 }
