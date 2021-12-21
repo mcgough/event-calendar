bd29973bbb994683c45346c6ef7135abe1830d97
Daniel McGough <daniel.e.mcgough@avanade.com>
2021-12-21 10:06:09 -0500

    enhance: day sub view transitions

--- frontend/src/views/sub-views/Day.vue
+++ frontend/src/views/sub-views/Day.vue
@@ -11,38 +11,45 @@
         </div>
       </div>
     </teleport>
-    <div class="w-14 flex flex-col justify-center items-center mt-4">
-      <div>
-        <span class="text-blue-500 font-medium">{{
-          DAYS_OF_WEEK_MEDIUM[dayInView.dayOfWeek]
-        }}</span>
-      </div>
-      <button
-        v-focus-on-mount
-        class="
-          w-12
-          h-12
-          rounded-full
-          bg-blue-500
-          text-white
-          flex
-          justify-center
-          items-center
-          font-medium
-        "
+    <transition :name="route.meta.transition">
+      <div
+        v-if="slideInDay"
+        class="w-14 flex flex-col justify-center items-center mt-4"
       >
-        <span class="text-lg">{{ dayInView.dayOfMonth }}</span>
-      </button>
-    </div>
-    <div v-for="event in events" :key="event.id" class="text-left">
-      {{ event.name }}
-    </div>
+        <div>
+          <div>
+            <span class="text-blue-500 font-medium">{{
+              DAYS_OF_WEEK_MEDIUM[dayInView.dayOfWeek]
+            }}</span>
+          </div>
+          <button
+            v-focus-on-mount
+            class="
+              w-12
+              h-12
+              rounded-full
+              bg-blue-500
+              text-white
+              flex
+              justify-center
+              items-center
+              font-medium
+            "
+          >
+            <span class="text-lg">{{ dayInView.dayOfMonth }}</span>
+          </button>
+        </div>
+        <div v-for="event in events" :key="event.id" class="text-left">
+          {{ event.name }}
+        </div>
+      </div>
+    </transition>
   </div>
 </template>
 
 <script>
 import PreviousNextAnchors from '@/components/PreviousNextAnchors.vue'
-import { computed, ref } from 'vue'
+import { computed, onMounted, ref } from 'vue'
 import { useCalendarRoutes } from '@/composables'
 import { DAYS_OF_WEEK_MEDIUM } from '@/constants'
 import { FocusOnMount } from '@/directives'
@@ -55,22 +62,61 @@ export default {
   setup() {
     const selectedDate = useSelectedDate()
 
-    const { constructPrevNextDayViewPaths, params } = useCalendarRoutes()
+    const { constructPrevNextDayViewPaths, params, route } = useCalendarRoutes()
 
     const dayInView = computed(() => selectedDate.day)
 
     const events = ref([])
 
+    const slideInDay = ref(false)
+
     const prevNextDayPaths = computed(() =>
       constructPrevNextDayViewPaths(params.value)
     )
+
+    onMounted(() => (slideInDay.value = true))
 
     return {
       dayInView,
       DAYS_OF_WEEK_MEDIUM,
       events,
       prevNextDayPaths,
+      route,
+      slideInDay,
     }
   },
 }
 </script>
+
+<style scoped>
+/* TODO: moved to css file */
+.day-slide-right-enter-active {
+  transition: all 0.5s ease;
+  transform: translateX(0rem);
+}
+.day-slide-right-leave-active {
+  position: absolute;
+}
+
+.day-slide-right-enter-from {
+  transform: translateX(-1rem);
+}
+.day-slide-right-leave-to {
+  opacity: 0;
+}
+
+.day-slide-left-enter-active {
+  transition: all 0.5s ease;
+  transform: translateX(0rem);
+}
+.day-slide-left-leave-active {
+  position: absolute;
+}
+
+.day-slide-left-enter-from {
+  transform: translateX(1rem);
+}
+.day-slide-left-leave-to {
+  opacity: 0;
+}
+</style>
